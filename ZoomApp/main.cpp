b7985c720a3bb0c0b0df2c945fdd681a19cab776
Frédéric Dubouchet <frederic.dubouchet@gmail.com>
2023-07-31 17:38:02 +0200

    Using the frame library.

--- ZoomApp/main.cpp
+++ ZoomApp/main.cpp
@@ -8,8 +8,23 @@
 
 #include "TinyImageLibrary/opencv_tiny_image.h"
 
+ABSL_FLAG(std::string, input_file, "", "Input file image to be process.");
+ABSL_FLAG(std::string, output_file, "", "Output file image to be saved.");
+
 int main(int ac, char** av) try {
+    absl::ParseCommandLine(ac, av);
     auto interface = std::make_unique<tiny_image::OpenCVTinyImage>();
+    if (absl::GetFlag(FLAGS_input_file).empty()) {
+           std::cerr << "Error: input_file or output_file is empty.\n";
+           return 1;
+       }
+    interface->Open(absl::GetFlag(FLAGS_input_file));
+
+    if (absl::GetFlag(FLAGS_output_file).empty()) {
+        std::cerr << "Error: input_file or output_file is empty.\n";
+    } else {
+        interface->Save(absl::GetFlag(FLAGS_output_file));
+    }
     return 0;
 } catch (std::exception& ex) {
     std::cerr << std::format("Exception: {}\n", ex.what());

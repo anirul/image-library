e759014bab940093a181065edd738c7ce37025c2
Frédéric Dubouchet <frederic.dubouchet@gmail.com>
2023-07-31 18:53:57 +0200

    Now it is showing a window (without anything in it).

--- ZoomApp/main.cpp
+++ ZoomApp/main.cpp
@@ -6,27 +6,60 @@
 #include <format>
 #include <memory>
 
+// From: https://sourceforge.net/p/predef/wiki/OperatingSystems/
+#if defined(_WIN32) || defined(_WIN64)
+#define WINDOWS_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
+#include "frame/common/application.h"
+#include "frame/file/file_system.h"
+#include "frame/file/image_stb.h"
+#include "frame/gui/draw_gui_factory.h"
+#include "frame/gui/input_factory.h"
+#include "frame/gui/window_camera.h"
+#include "frame/gui/window_cubemap.h"
+#include "frame/gui/window_resolution.h"
+#include "frame/json/parse_json.h"
+#include "frame/json/parse_level.h"
+#include "frame/json/proto.h"
+#include "frame/window_factory.h"
 #include "TinyImageLibrary/opencv_tiny_image.h"
 
 ABSL_FLAG(std::string, input_file, "", "Input file image to be process.");
+ABSL_FLAG(std::string, json_file, "", "JSON file for the windowing system.");
 ABSL_FLAG(std::string, output_file, "", "Output file image to be saved.");
 
 int main(int ac, char** av) try {
     absl::ParseCommandLine(ac, av);
-    auto interface = std::make_unique<tiny_image::OpenCVTinyImage>();
+    std::unique_ptr<tiny_image::TinyImageInterface> tiny_image_interface =
+        std::make_unique<tiny_image::OpenCVTinyImage>();
     if (absl::GetFlag(FLAGS_input_file).empty()) {
-           std::cerr << "Error: input_file or output_file is empty.\n";
-           return 1;
-       }
-    interface->Open(absl::GetFlag(FLAGS_input_file));
-
+        std::cerr << "Error: input_file is empty.\n";
+        return 1;
+    }
+    if (absl::GetFlag(FLAGS_json_file).empty()) {
+        std::cerr << "Error: json_file is empty.\n";
+        return 1;
+    }
+    tiny_image_interface->Open(absl::GetFlag(FLAGS_input_file));
+    frame::common::Application app(
+    frame::CreateNewWindow(
+        frame::DrawingTargetEnum::WINDOW,
+        frame::RenderingAPIEnum::OPENGL,
+        { 1280, 720 }));
+    app.Startup(frame::file::FindFile(absl::GetFlag(FLAGS_json_file)));
+    app.Run();
     if (absl::GetFlag(FLAGS_output_file).empty()) {
-        std::cerr << "Error: input_file or output_file is empty.\n";
-    } else {
-        interface->Save(absl::GetFlag(FLAGS_output_file));
+        std::cerr <<
+            "Error: output_file is empty the modification won't be saved.\n";
+    }
+    else {
+        tiny_image_interface->Save(absl::GetFlag(FLAGS_output_file));
     }
     return 0;
-} catch (std::exception& ex) {
+}
+catch (std::exception& ex) {
     std::cerr << std::format("Exception: {}\n", ex.what());
     return 1;
 }

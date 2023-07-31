a6e7ff59052aa5cf03ae76f4a73cd4cef2dac99e
Frédéric Dubouchet <frederic.dubouchet@gmail.com>
2023-07-31 11:09:45 +0200

    Minor modifs.

--- ZoomApp/main.cpp
+++ ZoomApp/main.cpp
@@ -1,5 +1,17 @@
+
+#include <absl/flags/flag.h>
+#include <absl/flags/parse.h>
+
+#include <iostream>
+#include <format>
+#include <memory>
+
 #include "TinyImageLibrary/opencv_tiny_image.h"
 
-int main(int ac, char** av) {
+int main(int ac, char** av) try {
+    auto interface = std::make_unique<tiny_image::OpenCVTinyImage>();
     return 0;
+} catch (std::exception& ex) {
+    std::cerr << std::format("Exception: {}\n", ex.what());
+    return 1;
 }
